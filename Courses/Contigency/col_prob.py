545d467bafdf3dd5c096858f3576b14ed049da04
sun-te <sun_te@outlook.com>
2019-12-30 00:56:59 +0800

    compute proba & compare

--- Courses/Contigency/col_prob.py
+++ Courses/Contigency/col_prob.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse, Polygon, Rectangle
 import matplotlib.transforms as transforms
-from util import confidence_ellipse, validation_rotation
+from util import confidence_ellipse, validation_rotation, rotation_matrix, close_polygone
+from util import compute_proba, monteCarlo
 from ipdb import set_trace as tt
-POS_RATIO = 0.7
+POS_RATIO = 0.3
 
-def rotation_matrix(theta):
-    return np.array([[np.cos(theta), -np.sin(theta)],
-              [np.sin(theta), np.cos(theta)]])
+
+
 class Object:
     def __init__(self, x, y, length, width, angle):
         self.cx = x
@@ -52,10 +52,12 @@ class Robot(Object):
         pts = np.concatenate([self.corner, self.corner[0].reshape(1, 2)], axis=0).T
         x, y = pts[0], pts[1]
         ax.plot(x, y, label="Robot")
+        ax.scatter(self.cx, self.cy)
 
 class Obstacle(Object):
     def __init__(self, x, y, length, width, angle, pos_cov=None, ang_var=None):
         super(Obstacle, self).__init__(x, y, length, width, angle)
+        self.ratio = POS_RATIO
         self.pos_cov = pos_cov
         self.ang_var = ang_var
         self.rot_matrix = None
@@ -88,25 +90,30 @@ class Obstacle(Object):
         confidence_ellipse(x, y, ax, edgecolor='red')
         ax.scatter(m[0], m[1])
         plt.show()
-        m = self.W@m
+        m = self.W @ m
         x = m[0]
         y = m[1]
         ax.scatter(m[0], m[1])
         confidence_ellipse(x, y, ax, edgecolor='red')
         plt.show()
 
-    def plot(self, ax=None):
+    def plot(self, ax=None, transformer=np.eye(2)):
         if ax is None:
             figure = plt.figure()
             ax = figure.add_subplot(111, aspect='equal')
         pts = np.concatenate([self.corner, self.corner[0].reshape(1, 2)], axis=0).T
+        pts = transformer.T @ (pts - np.array([[self.cx], [self.cy]])) + np.array([[self.cx], [self.cy]])
         x, y = pts[0], pts[1]
-        plt.plot(x, y, label='Obstacle')
+        ax.plot(x, y)
         pts1, pts2 = self.generate(self.angle - self.ang_var), self.generate(self.angle + self.ang_var)
+
         pts1 = np.concatenate([pts1, pts1[0].reshape(1, 2)], axis=0).T
         pts2 = np.concatenate([pts2, pts2[0].reshape(1, 2)], axis=0).T
+        pts1, pts2 = transformer.T @ (pts1 - np.array([[self.cx], [self.cy]])) + np.array([[self.cx], [self.cy]]), \
+                     transformer.T @ (pts2 - np.array([[self.cx], [self.cy]])) + np.array([[self.cx], [self.cy]])
         ax.plot(pts1[0], pts1[1])
         ax.plot(pts2[0], pts2[1])
+        ax.scatter(self.cx, self.cy)
         cov = self.pos_cov
         pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
         # Using a special case to obtain the eigenvalues of this
@@ -154,13 +161,13 @@ class Obstacle(Object):
         return box
 
 
-
 class Collision:
     def __init__(self):
         self.robot = None
         self.obstacle = None
         self.cb_box = None
         self.demo_robot = None
+        self.robot_corner = None
 
     def cbPolygon(self, robot_corner=None, plot=False, ax=None):
         if robot_corner is None:
@@ -169,6 +176,7 @@ class Collision:
             assert self.robot is not None, "Must provide a robot shape"
 
             robot_corner = self.robot.corner
+        self.robot_corner = robot_corner
         cx = np.mean(robot_corner[:, 0])
         cy = np.mean(robot_corner[:, 1])
         robot_plot = None
@@ -221,12 +229,13 @@ class Collision:
             else:
                 if y - cy > 0 and x - cx > 0:
                     cb_pts.append([x + obst_box["left"], y + obst_box["down"]])
+                    self.demo_obst = Obstacle(x + obst_box["left"], y + obst_box["down"],
+                                              width=self.obstacle.width, length=self.obstacle.length,
+                                              angle=self.obstacle.angle * 180 / np.pi)
                     if plot:
                         ax.add_artist(Rectangle(xy=(x, y), width=w, height=h,
                                                 edgecolor='green', linestyle='--', facecolor='none'))
-                        self.demo_obst = Obstacle(x + obst_box["left"], y + obst_box["down"],
-                                                  width=self.obstacle.width, length=self.obstacle.length,
-                                                  angle=self.obstacle.angle * 180 / np.pi)
+
                         self.demo_obst.plot(ax)
                 if y - cy > 0 and x - cx < 0:
                     cb_pts.append([x - obst_box["right"], y + obst_box["down"]])
@@ -258,37 +267,173 @@ class Collision:
 
         return self.cb_box
 
-    def transformed_cb(self, plot=False):
+    def transformed_cb(self, plot=False, ax1=None, ax2=None):
+        """
+
+        :param plot:
+        :param ax1:
+        :param ax2:
+        :return: the rotated bounding box of the combined body and the transformed center of the obstacle
+        """
+        W = self.obstacle.W
+
+        # min_x, min_y, width, height
+        bbx = min(self.cb_box[:,0]), min(self.cb_box[:,1]), max(self.cb_box[:,0]) - min(self.cb_box[:,0]), \
+                  max(self.cb_box[:,1]) - min(self.cb_box[:,1])
+
+        bounding_box_corner = np.array([[bbx[0], bbx[1]],
+                                        [bbx[0], bbx[1] + bbx[3]],
+                                        [bbx[0] + bbx[2], bbx[1] + bbx[3]],
+                                        [bbx[0] + bbx[2], bbx[1]]
+                                        ])
+
+        self.cb_box_transformed = self.cb_box @ W.T
+        cb_box_t = np.concatenate([self.cb_box_transformed, self.cb_box_transformed[0].reshape(1, 2)], axis=0)
+        pg = Polygon(xy=cb_box_t, closed=True, facecolor="None", edgecolor='b', fill=False, linestyle='--')
+
+        robot_plot = self.robot_corner @ W.T
+        trans_robot = robot_plot
+        robot_plot = np.concatenate([robot_plot, robot_plot[0].reshape(1, 2)], axis=0)
+        robot_plot = Polygon(xy=robot_plot, closed=True, facecolor="blue", edgecolor='b', fill=True,
+                             alpha=0.3, linestyle='-')
+
+        demo_center = np.array([self.demo_obst.cx, self.demo_obst.cy]) @ W.T
+        demo_obst = Obstacle(demo_center[0], demo_center[1],
+                             width=self.obstacle.width, length=self.obstacle.length,
+                             angle=self.obstacle.angle * 180 / np.pi,
+                             pos_cov=W@self.obstacle.pos_cov@W.T)
 
 
+        transformed_bbx = bounding_box_corner @ W.T
+        plot_transformed_bbx = Polygon(xy=transformed_bbx, closed=True, facecolor="None", edgecolor='b',
+                                       linestyle='--')
 
-        return
+        # compute angle
+        line = np.array([1, 0]).reshape(1, 2)
+        line = line @ W.T
+        theta = np.arctan(line[0, 1] / line[0, 0])
+        rot_matrix = rotation_matrix(- theta)
+        # re-rotated combined body and its bounding box
+        rot_transform_cb = cb_box_t @ rot_matrix.T
+        rot_pg = Polygon(xy=rot_transform_cb, closed=True, facecolor="None", edgecolor='b', fill=False, linestyle='--')
+        rot_bbx = min(rot_transform_cb[:,0]), min(rot_transform_cb[:,1]), \
+                  max(rot_transform_cb[:,0]) - min(rot_transform_cb[:,0]), \
+                  max(rot_transform_cb[:,1]) - min(rot_transform_cb[:,1])
+        bounding_box_corner = np.array([[rot_bbx[0], rot_bbx[1]],
+                                        [rot_bbx[0], rot_bbx[1] + rot_bbx[3]],
+                                        [rot_bbx[0] + rot_bbx[2], rot_bbx[1] + rot_bbx[3]],
+                                        [rot_bbx[0] + rot_bbx[2], rot_bbx[1]]
+                                        ])
+        plot_rotated_bbx = Polygon(xy=bounding_box_corner, closed=True, facecolor="None", edgecolor='orange',
+                                       linestyle='--')
+        demo_center = np.array([self.obstacle.cx, self.obstacle.cy]) @ W.T @ rot_matrix.T
+        demo_obst_rot = Obstacle(demo_center[0], demo_center[1],
+                             width=self.obstacle.width, length=self.obstacle.length,
+                             angle=self.obstacle.angle * 180 / np.pi,
+                             pos_cov=W @ self.obstacle.pos_cov @ W.T)
+
+
+        # TODO: bouding box for the shape
+        if plot:
+            ax1.add_artist(pg)
+            demo_obst.plot(ax1, transformer=W)
+            ax1.add_artist(robot_plot)
+            # ax.add_artist(plot_transformed_bbx)
+            ax1.legend([robot_plot, pg], ["transformed robot", 'transformed cb polygon'])
+            ax2.add_artist(plot_rotated_bbx)
+            ax2.add_artist(rot_pg)
+            demo_obst_rot.plot(ax2, transformer=W)
+            ax2.legend([plot_rotated_bbx, rot_pg], ["bounding box", 'rotated cb polygon'])
+        print("Collision probability upper bound: {}".format(compute_proba(bounding_box_corner, demo_center)))
+        return bounding_box_corner, demo_center
 
 
 
 if __name__ == "__main__":
-    # Firstly, let's verify some functionality
+    # # Firstly, let's verify some functionality
+    # # Idealy, the transformed data should be of covariance of
+    # # Identity matrix, which means that the equal probability line should be
+    # # a circle
     # for i in range(3):
     #     validation_rotation()
+    #
+    # ##########################################################
+    # # This part is for the validation of the implementation  #
+    # # for probability upper bound computation                #
+    # ##########################################################
+    # figure = plt.figure(figsize=[15,5])
+    # ax = figure.add_subplot(131, aspect='equal')
+    # ax.set_xlim([-10, 10])
+    # ax.set_ylim(  [-10, 10])
+    # ax.set_title("Figure with demo obstacle")
+    # collision = Collision()
+    # collision.robot = Robot(0, 0, 4.5, 2, 0)
+    # collision.obstacle = Obstacle(9, 8, 4.5, 2, 45)
+    # # collision.obstacle.plot(ax)
+    # # collision.robot.plot(ax)
+    # corner = np.array([[1, 0],
+    #                    [0.5, -np.sqrt(3)/2],
+    #                    [-0.5, -np.sqrt(3)/2],
+    #                    [-1, 0],
+    #                    [-0.5, np.sqrt(3)/2],
+    #                    [0.5, np.sqrt(3)/2]]) * 5
+    # # a hexagon like robot area
+    # collision.cbPolygon(robot_corner=corner, ax=ax, plot=True)
+    # # de-comment the line below to have a rectangular robot
+    # # collision.cbPolygon(ax=ax, plot=True)
+    # ax2 = figure.add_subplot(132, aspect='equal')
+    # ax2.set_xlim([-7, 7])
+    # ax2.set_ylim([-7, 7])
+    # ax2.set_title("Figure with demo obstacle")
+    # ax3 = figure.add_subplot(133, aspect='equal')
+    # ax3.set_title("Figure with real obstacle")
+    # ax3.set_xlim([-7, 7])
+    # ax3.set_ylim([-7, 7])
+    # collision.transformed_cb(plot=True, ax1=ax2, ax2=ax3)
+    # plt.show()
 
-    figure = plt.figure()
-    ax = figure.add_subplot(111, aspect='equal')
-    ax.set_xlim([-10, 10])
-    ax.set_ylim([-10, 10])
+    #########################################
+    # This part is for the computation of   #
+    # the probability during time steps     #
+    #########################################
+    width = 2
+    length = 4
     collision = Collision()
-    collision.robot = Robot(0, 0, 10,3, 0)
-    collision.obstacle = Obstacle(2, 3, 4.5, 2, 45)
+    collision.robot = Robot(0, 0, width=width, length=length, angle=0)
+
+    figure = plt.figure(figsize=[8, 6])
+    ax = figure.add_subplot(111, aspect='equal')
+    ax.set_xlim([-6, 10])
+    ax.set_ylim([-6, 6])
+    collision.robot.plot(ax)
+    initial_state = [8, 3, 180]
+    collision.obstacle = Obstacle(initial_state[0], initial_state[1],
+                                  width=width, length=length,
+                                  angle=initial_state[2])
     # collision.obstacle.plot(ax)
-    # collision.robot.plot(ax)
-    corner = np.array([[1, 0],
-                       [0.5, -np.sqrt(3)/2],
-                       [-0.5, -np.sqrt(3)/2],
-                       [-1, 0],
-                       [-0.5, np.sqrt(3)/2],
-                       [0.5, np.sqrt(3)/2]]) * 5
-    collision.cbPolygon(robot_corner=corner, ax=ax, plot=True)
-    # collision.plot_cbPolygon(ax)
-    # ax.legend()
+
+    position = np.array([initial_state[:2],
+                         [6, 3], [4, 3], [2, 3],
+                         [0.2, 2.8], [-0.5, 2.5], [-1.7, 1.5],
+                         [-2.2, 0.5], [-2.2, -1], [-2.2, -2.5],
+                         ])
+    # tt()
+    angle = np.array([0, 0, 0, 0, 30, 40, 60, 70, 80 ,90 ,90]) + 180
+    # uncertainty = np.exp(np.linspace(0.1, 1.5, num=10)) * 0.25
+    uncertainty = np.ones(10) * 0.5
+    robot_corner = collision.robot.corner
+    for i in range(6, len(position)):
+        collision.obstacle = Obstacle(position[i, 0], position[i, 1],
+                                  width=width, length=length,
+                                  angle=angle[i])
+        collision.obstacle.pos_cov *= uncertainty[i] ** 2
+        pos_cov = collision.obstacle.pos_cov
+        collision.obstacle.ang_var *= uncertainty[i]
+        real_proba = monteCarlo(position[i, 0], position[i,1], pos_cov, robot_corner, ax=ax)
+        print("Real probability : {}".format(real_proba))
+        collision.obstacle.plot(ax)
+        collision.cbPolygon()
+        collision.transformed_cb()
+
+    ax.set_title("Robot-Obstacle Collision Detection")
     plt.show()
-
-
--- Courses/Contigency/util.py
+++ Courses/Contigency/util.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
+from scipy.special import erf
+
 
 def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
     """
@@ -91,3 +93,48 @@ def validation_rotation():
     ax2.scatter(m[0], m[1], s=5)
     confidence_ellipse(m[0], m[1], ax2, edgecolor='red')
     plt.show()
+
+def rotation_matrix(theta):
+    return np.array([[np.cos(theta), -np.sin(theta)],
+              [np.sin(theta), np.cos(theta)]])
+
+def close_polygone(arr):
+    if arr.shape[1] != 2:
+        arr.reshape([-1, 2])
+    pts = np.concatenate([arr, arr[0].reshape(1, 2)], axis=0)
+    return pts
+
+def phi(z):
+    return 1/2 * (1 + erf(z/np.sqrt(2)))
+
+def compute_proba(cb, obst_center):
+    """
+    return the probability upper bound defined in the article (Page919), equation (17)
+    :param cb:
+    :param obst_center:
+    :return:
+    """
+    if cb.shape[1] != 2:
+        cb = cb.T
+    x, y = obst_center[0], obst_center[1]
+    max_x, min_x = np.max(cb[:, 0]), np.min(cb[:, 0])
+    max_y, min_y = np.max(cb[:, 1]), np.min(cb[:, 1])
+    return (phi(max_x - x) - phi(min_x - x)) * (phi(max_y - y) - phi(min_y - y))
+
+def monteCarlo(x, y, cov, corner, num=50000, ax=None):
+    # TODO: Not the center cross but the edge cross should be counted
+    pts = np.random.multivariate_normal([x, y], cov=cov, size=num)
+    ax.scatter(pts.T[0], pts.T[1], s=1)
+    max_x, min_x = np.max(corner[:, 0]), np.min(corner[:, 0])
+    max_y, min_y = np.max(corner[:, 1]), np.min(corner[:, 1])
+    # import ipdb
+    # ipdb.set_trace()
+    def in_robot(z):
+        if (z[0] > min_x) and (z[0] < max_x) and (z[1] < max_y) and (z[1] > min_y):
+            return 1
+        else:
+            return 0
+    res = 0
+    for pt in pts:
+        res += in_robot(pt)
+    return res / num
